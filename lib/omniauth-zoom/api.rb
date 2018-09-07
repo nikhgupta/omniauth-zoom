870797c037bdd20da8ff0d23f8be266441b95b6c
Nikhil Gupta <me@nikhgupta.com>
2018-09-07 16:02:50 +0530

    snapped

--- lib/omniauth-zoom/api.rb
+++ lib/omniauth-zoom/api.rb
@@ -9,16 +9,44 @@ module Omniauth
       def initialize(token)
         @access_token = token
         self.class.headers ||= {}
+        binding.pry
         self.class.headers['Authorization'] = "Bearer #{@access_token}"
       end
 
-      class_eval do
-        HTTParty::Request::SupportedHTTPMethods.each do |klass|
-          method = klass.demodulize.underscore
-          define_method(method) do |*args, &block|
-            self.class.send(method, *args, &block)
-          end
-        end
+      def get(*args, &block)
+        self.class.get(*args, &block)
+      end
+
+      def post(*args, &block)
+        self.class.post(*args, &block)
+      end
+
+      def patch(*args, &block)
+        self.class.patch(*args, &block)
+      end
+
+      def put(*args, &block)
+        self.class.put(*args, &block)
+      end
+
+      def delete(*args, &block)
+        self.class.delete(*args, &block)
+      end
+
+      def head(*args, &block)
+        self.class.head(*args, &block)
+      end
+
+      def options(*args, &block)
+        self.class.options(*args, &block)
+      end
+
+      def move(*args, &block)
+        self.class.move(*args, &block)
+      end
+
+      def copy(*args, &block)
+        self.class.copy(*args, &block)
       end
     end
   end
